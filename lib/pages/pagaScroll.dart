7e0758dc3382c293b59ed76025bbdfc9a1f549e3
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-05-14 23:04:28 -0400

    pantalla 2 de pageView

--- lib/pages/pagaScroll.dart
+++ lib/pages/pagaScroll.dart
@@ -17,14 +17,34 @@ class PageScroll extends StatelessWidget {
   }
 
   Widget _page1() {
-    return Center(
-      child: Text("Hola Mundo 1"),
+    return SafeArea(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            color: Colors.lightBlue,
+          ),
+          Image(
+            image: AssetImage('assets/scroll.png'),
+            width: double.infinity, 
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Column(
+            children: <Widget>[
+              Text("11°",style: TextStyle(fontSize: 50,color: Colors.white),),
+              Text("Miercoles",style: TextStyle(fontSize: 30,color: Colors.white),),
+              Expanded(child: Container()),
+              Icon(Icons.keyboard_arrow_down,size: 60,color: Colors.white,)
+            ],
+          )
+        ],
+      ),
     );
   }
 
   Widget _page2() {
     return Center(
-      child: Text("Hola Mundo 2"),
+      child: null,
     );
   }
 }
