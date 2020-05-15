94c18990f064dea9dfdeb205b776b3447d3c8fc9
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-05-15 00:09:53 -0400

    utilizamos por primera vez gradiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practica_flutter/pages/botonesPage.dart';
 import 'package:practica_flutter/pages/pagaScroll.dart';
 import 'package:practica_flutter/pages/page1.dart';
 
@@ -11,10 +12,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/pageScroll',
+      initialRoute: '/pageBotones',
       routes: {
         '/' : (BuildContext context) => page1(),
         '/pageScroll' : (BuildContext context) => PageScroll(),
+        '/pageBotones' : (BuildContext context) => BotonoesPage(),
       },
     );
   }
