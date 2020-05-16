9f45eb61510a966fb03e85ab09b162b1cf5c4827
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-05-15 21:40:58 -0400

    bottombar con 3 iconos

--- lib/pages/botonesPage.dart
+++ lib/pages/botonesPage.dart
@@ -14,9 +14,39 @@ class BotonoesPage extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           _fondo(),
-          _titulos(),
+          SingleChildScrollView(
+            child: _titulos(),
+          )
+          
         ],
       ),
+      bottomNavigationBar: _bottonNavigatonBar(context),
+    );
+  }
+
+  Widget _bottonNavigatonBar(BuildContext context){
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+        primaryColor: Colors.pinkAccent,  
+        textTheme: Theme.of(context).textTheme.copyWith(caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0)))
+      ), 
+      child: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            title: Container(),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bubble_chart),
+            title: Container(),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle),
+            title: Container(),
+          ),
+        ]
+      )
     );
   }
 
